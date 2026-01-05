e91c7f3beebf5ac98c3ea2f8e5a36174307a744b
neggmmm <kekkdead@gmail.com>
2026-01-05 23:12:02 +0200

    AppConfig completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/user.module';
+import { ConfigService } from '@nestjs/config';
+import { AppConfigModule } from './config/config.services';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UserModule],
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      })
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
