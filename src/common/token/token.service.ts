c523eba6ac5ad0d1e7f9ccfd8893373578e00b52
neggmmm <kekkdead@gmail.com>
2026-01-09 15:58:21 +0200

    send userId to jwt.sign

--- src/common/token/token.service.ts
+++ src/common/token/token.service.ts
@@ -4,9 +4,16 @@ import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class TokenService {
-    constructor(private readonly config: ConfigService) {}
+    constructor(private readonly config: ConfigService) { }
     generateToken(userId: string): string {
-        return jwt.sign({sub : userId}, this.config.getOrThrow<string>('JWT_ACCESS_SECRET'), { expiresIn: '15m' });
+        return jwt.sign(
+            {},
+            this.config.getOrThrow<string>('JWT_ACCESS_SECRET'),
+            {
+                subject: userId,
+                expiresIn: '15m',
+            },
+        );
     }
 
     verifyToken(token: string): any {
